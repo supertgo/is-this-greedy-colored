732a38e936f2e985ef9aca99ed481dccac544545
supertgo <thiagoroberto2003@gmail.com>
2023-11-06 13:48:49 -0300

    feat: add sorth methods

--- include/sort.h
+++ include/sort.h
@@ -76,9 +76,124 @@ template <typename T> void quick_sort(T *items, int size) {
   sort(0, size - 1, items);
 }
 
-template <typename T> void merge_sort(T *items, int size) {}
+template <typename T> void merge(T *items, int left, int mid, int right) {
+  int i, j, k;
+  int n1 = mid - left + 1;
+  int n2 = right - mid;
 
-template <typename T> void heap_sort(T *items, int size) {}
+  T *leftArr = new T[n1];
+  T *rightArr = new T[n2];
+
+  for (i = 0; i < n1; i++)
+    leftArr[i] = items[left + i];
+  for (j = 0; j < n2; j++)
+    rightArr[j] = items[mid + 1 + j];
+
+  i = 0;
+  j = 0;
+  k = left;
+  while (i < n1 && j < n2) {
+    if (leftArr[i] <= rightArr[j]) {
+      items[k] = leftArr[i];
+      i++;
+    } else {
+      items[k] = rightArr[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    items[k] = leftArr[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    items[k] = rightArr[j];
+    j++;
+    k++;
+  }
+
+  delete[] leftArr;
+  delete[] rightArr;
+}
+
+template <typename T> void merge_sort_helper(T *items, int left, int right) {
+  if (left < right) {
+    int mid = left + (right - left) / 2;
+
+    merge_sort_helper(items, left, mid);
+    merge_sort_helper(items, mid + 1, right);
+
+    merge(items, left, mid, right);
+  }
+}
+
+template <typename T> void merge_sort(T *items, int size) {
+  merge_sort_helper(items, 0, size - 1);
+}
+
+template <typename T> void heapify(T *items, int size, int root) {
+  int largest = root;
+  int left = 2 * root + 1;
+  int right = 2 * root + 2;
+
+  if (left < size && items[left] > items[largest])
+    largest = left;
+
+  if (right < size && items[right] > items[largest])
+    largest = right;
+
+  if (largest != root) {
+    swap(items[root], items[largest]);
+
+    heapify(items, size, largest);
+  }
+}
+
+template <typename T> void heap_sort(T *items, int size) {
+  for (int i = size / 2 - 1; i >= 0; i--)
+    heapify(items, size, i);
+
+  for (int i = size - 1; i > 0; i--) {
+    swap(items[0], items[i]);
+
+    heapify(items, i, 0);
+  }
+}
+
+template <typename T>
+void cocktail_sort(T *items, int size) {
+  int swapped = true;
+  int start = 0;
+  int end = size - 1;
+
+  while (swapped) {
+    swapped = false;
+
+    for (int i = 0; i < end; i++) {
+      if (items[i] > items[i + 1]) {
+        swap(items[i], items[i + 1]);
+        swapped = true;
+      }
+    }
+
+    if (!swapped)
+      break;
+
+    swapped = false;
+
+    for (int i = end - 1; i >= start; --i) {
+      if (items[i] > items[i + 1]) {
+        swap(items[i], items[i + 1]);
+        swapped = true;
+      }
+    }
+
+    ++start;
+  }
+}
 
 template <typename T> void swap(T &x, T &y) {
   T temp = x;
